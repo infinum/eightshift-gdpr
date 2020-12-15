37519a80d725d365c6355c4ebb473b8dfe445ecd
Denis Žoljom <denis.zoljom@gmail.com>
2020-12-15 20:30:38 +0100

    Add missing exceptions

---
+++ src/Exception/GeneralException.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * File containing general exception interface
+ *
+ * @package Eightshift\EightshiftGdpr\Exception
+ * @since 1.1.0
+ */
+
+declare(strict_types=1);
+
+namespace Eightshift\EightshiftGdpr\Exception;
+
+/**
+ * Interface GeneralException
+ *
+ * This interface is implemented by all exceptions, so that we can catch "internal" exceptions only.
+ *
+ * @package Eightshift\EightshiftGdpr\Exception
+ * @since 1.1.0
+ */
+interface GeneralException
+{
+
+}
---
+++ src/Exception/InvalidUri.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * File containing the invalid URI exception class
+ *
+ * @package Eightshift\EightshiftGdpr\Exception
+ * @since 1.1.0
+ */
+
+declare(strict_types=1);
+
+namespace Eightshift\EightshiftGdpr\Exception;
+
+use InvalidArgumentException;
+
+/**
+ * Invalid uri exception
+ *
+ * Exception thrown when a URI is not valid in WordPress context.
+ *
+ * @package Eightshift\EightshiftGdpr\Exception
+ * @since 1.1.0
+ */
+final class InvalidUri extends InvalidArgumentException implements GeneralException
+{
+	/**
+	* Create a new instance of the exception for a file that is not accessible
+	* or not readable.
+	*
+	* @param string $uri URI of the file that is not accessible or not
+	*                    readable.
+	*
+	* @return static
+	*/
+	public static function fromUri(string $uri): InvalidUri
+	{
+		$message = sprintf('The View URI "%s" is not accessible or readable.', $uri);
+
+		return new InvalidUri($message);
+	}
+}

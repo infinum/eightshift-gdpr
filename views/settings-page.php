87b07cc29fb006323b265390d663acdc030b76f0
Denis Žoljom <denis.zoljom@gmail.com>
2020-12-15 20:30:53 +0100

    Add settings page view placeholder

---
+++ views/settings-page.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Settings page wrapper
+ *
+ * Used for displaying all the Woo Solo Api plugin settings
+ *
+ * @since 1.1.0
+ * @package Eightshift\EightshiftGdpr\Views
+ */
+
+namespace Eightshift\EightshiftGdpr\Views;
+
+?>
+<div class="wrap">
+	<div id="eightshift-gdpr-settings-page"></div>
+</div>
